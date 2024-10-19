5511155aac30797311c49d31729b9b8f62137397
Jorge Loayza <50973521+robcuero@users.noreply.github.com>
2024-10-19 11:31:53 -0500

    Create users.test.js

---
+++ users.test.js
@@ -0,0 +1,11 @@
+test('POST /api/users debe crear un nuevo usuario', () => {
+	expect(true).toBe(true);
+});
+
+test('GET /api/users debe devolver una lista de usuarios', () => {
+	expect(true).toBe(true);
+});
+
+test('POST /api/users debe devolver error si el email ya existe', () => {
+	expect(true).toBe(true);
+});
